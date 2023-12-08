8ad59bdf8fef94d3b6efb779e23061c4dbf3c637
mforte215 <mark.fortee@gmail.com>
2023-12-08 11:40:01 -0500

    updated the navbar not to overlap. Added the mock data and new logo

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,13 +1,50 @@
+/********************/
+/* Global Functions* /
+/******************/
+
 document.addEventListener('DOMContentLoaded', () => {
 
-    // Get "navbar-burger" element
+    // Get "navbar-burger" and "navbar-menu" elements
     const navbarBurger = document.querySelector('.navbar-burger');
     const navbarMenu = document.querySelector('.navbar-menu');
-    // Add a click event on each of them
-
     navbarBurger.addEventListener('click', (event) => {
         event.preventDefault();
         navbarBurger.classList.toggle('is-active');
         navbarMenu.classList.toggle('is-active');
     });
 });
+
+
+/********************/
+/* Fetch Functions* /
+/******************/
+
+const searchForMovies = async () => {
+    //TODO Get the search values out of the controls the results and make sure the text box is not blank.
+    let mediaSearchTextInput = document.querySelector('#media-input');
+
+
+
+    const url = 'https://movie-database-alternative.p.rapidapi.com/?s=' + searchedMovie + '&r=json&page=1';
+    const options = {
+        method: 'GET',
+        headers: {
+            'X-RapidAPI-Key': API_KEY,
+            'X-RapidAPI-Host': 'movie-database-alternative.p.rapidapi.com'
+        }
+    };
+
+    try {
+        const response = await fetch(url, options);
+        const result = await response.json();
+        console.log(result);
+
+        //TODO Place the results into the DOM with the Document Object
+
+
+    } catch (error) {
+        console.error(error);
+    }
+
+}
+
